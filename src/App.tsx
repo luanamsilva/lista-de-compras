7f841ec3bdd8fc9abb1ac0047fd7dd492cc2cfa0
luanamsilva <luanamoura2002@yahoo.com.br>
2023-05-28 19:55:08 -0300

    add-localstorage

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import {Header} from './components/Header'
 import {NoItems} from './components/NoItems'
-import {useState} from 'react'
+import {useEffect,useState} from 'react'
 import {Items} from './components/Items'
 import {ItemsProps} from './types/ItemsProps'
 import cart from './assets/16246.jpg'
@@ -8,21 +8,36 @@ import styles from './App.module.css'
 
 function App() {
 
+  const LOCAL_STORAGE_KEY = 'item:savedItem'
   const [listItemAdd, setListItemAdd] = useState<ItemsProps[]>([])
 
+  function loadLocalStorage() {
+    const saved = localStorage.getItem(LOCAL_STORAGE_KEY)
+    if(saved){
+      setListItemAdd(JSON.parse(saved))
+    }
+  }
 
+  function setItemsAndSave(newItems:ItemsProps[] ) {
+    setListItemAdd(newItems)
+    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(newItems))
+  }
+
+  useEffect(()=>{
+    loadLocalStorage();
+  },[])
   function handleAddItem(itemTitle:string) {
     const newListItemAdd = [...listItemAdd];
     newListItemAdd.push({
       id: listItemAdd.lehgth +1,
       title: itemTitle
     })
-    setListItemAdd(newListItemAdd)
+    setItemsAndSave(newListItemAdd)
   }
 
   function onDelete(itemTitle:string) {
     const newItemDelete = listItemAdd.filter((item) => item.title !== itemTitle)
-    setListItemAdd(newItemDelete)
+    setItemsAndSave(newItemDelete)
   }
   return (
     <>
